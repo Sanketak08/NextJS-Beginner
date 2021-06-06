88c42a979c7de2b30bcd6bfa9ba247db915e4a57
Sanket Kulkarni <sakulkarni0801@gmail.com>
2021-06-06 12:41:22 +0530

    NextJS basic functionalities done

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,16 @@
 import styles from "../styles/Layout.module.css";
 import Header from "./Header";
+import Meta from "./Meta";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <main className={styles.main}>{children}</main>
-    </div>
+    <>
+      <Meta />
+      <div className={styles.container}>
+        <Header />
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
   );
 };
 
